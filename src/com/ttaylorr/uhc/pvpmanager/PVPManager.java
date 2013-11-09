211d94f6bb869e28a5fbd79ce8f2ff646b9105bb
Taylor Blau <tblau16@cranbrook.edu>
2013-11-09 17:30:26 -0500

    Initial commit for new version

---
+++ src/com/ttaylorr/uhc/pvpmanager/PVPManager.java
@@ -0,0 +1,17 @@
+package com.ttaylorr.uhc.pvpmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PVPManager extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+}
